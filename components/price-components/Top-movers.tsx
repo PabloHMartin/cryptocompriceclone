e666c4e41d5c322fa89f54f21375b25f9c508b61
Pablo Hernandez Martin <pablohmartindev@gmail.com>
2022-03-24 19:20:36 +0100

    minichart and responsive layout

--- components/price-components/Top-movers.tsx
+++ components/price-components/Top-movers.tsx
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import Image from "next/image";
 import { SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
+import { useWindowSize } from "../../lib/hooks/useWindowSize";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/autoplay";
@@ -19,7 +20,11 @@ import {
   CardBottom,
   CardAssetName,
   CardAssetRate,
+  CardAssetNameWrapper,
+  MinichartWrapper,
 } from "../../styles/top-mover-styles";
+import Link from "next/link";
+import MiniChart from "./Mini-chart";
 
 export default function TopMovers({ topMovers }: { topMovers: TopMover[] }) {
   return (
@@ -33,10 +38,12 @@ export default function TopMovers({ topMovers }: { topMovers: TopMover[] }) {
 }
 
 const SwiperOpts = ({ topMovers }: { topMovers: TopMover[] }) => {
+  const size = useWindowSize();
+
   return (
     <SwiperCustom
       spaceBetween={50}
-      slidesPerView={2.5}
+      slidesPerView={size.width > 1440 ? 4 : 2.5}
       loop={true}
       autoplay={{
         delay: 3000,
@@ -56,29 +63,45 @@ const SwiperOpts = ({ topMovers }: { topMovers: TopMover[] }) => {
 };
 
 const CardOption = ({ mover }: { mover: TopMover }) => {
+  const size = useWindowSize();
   return (
-    <Card sx={{ minWidth: 150 }} elevation={0}>
-      <CardContentStyled>
-        <CardTop>
-          {mover.icon ? (
-            <Image src={mover.icon} alt="asset logo" width={24} height={24} />
-          ) : (
-            <Image
-              src="/color_icon.png"
-              alt="asset logo"
-              width={24}
-              height={24}
-            />
-          )}
+    <Link href={`price/${mover.slug}`} passHref>
+      <Card sx={{ minWidth: 150 }} elevation={0}>
+        <CardContentStyled>
+          <CardTop>
+            {mover.icon ? (
+              <Image src={mover.icon} alt="asset logo" width={24} height={24} />
+            ) : (
+              <Image
+                src="/color_icon.png"
+                alt="asset logo"
+                width={24}
+                height={24}
+              />
+            )}
 
-          <CardAssetRateDiff>10.85%</CardAssetRateDiff>
-        </CardTop>
-        <CardBottom>
-          <CardAssetName>{mover.name}</CardAssetName>
-          <div>{mover.symbol}</div>
-          <CardAssetRate>${mover.usd_price.toFixed(2)}</CardAssetRate>
-        </CardBottom>
-      </CardContentStyled>
-    </Card>
+            {size.width < 1440 ? (
+              <CardAssetRateDiff>10.85%</CardAssetRateDiff>
+            ) : (
+              <MinichartWrapper>
+                <MiniChart prices={mover.prices} />
+              </MinichartWrapper>
+            )}
+          </CardTop>
+          <CardBottom>
+            <CardAssetNameWrapper>
+              <CardAssetName>{mover.name}</CardAssetName>
+              <div>{mover.symbol}</div>
+            </CardAssetNameWrapper>
+            <CardAssetRate>
+              <div>${mover.usd_price.toFixed(2)} </div>
+              {size.width > 1440 && (
+                <CardAssetRateDiff>10.85%</CardAssetRateDiff>
+              )}
+            </CardAssetRate>
+          </CardBottom>
+        </CardContentStyled>
+      </Card>
+    </Link>
   );
 };
